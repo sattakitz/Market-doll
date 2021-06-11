f99c5462703e626749f07292701a6c63557b3d3b
sattakitz <sattakit.kacs60@ubru.ac.th>
2021-06-11 21:54:51 +0700

    login user

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formlogin!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private dataService: AppService,
+    private router: Router) {
+    this.formlogin = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(1)]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formlogin.value);
+
+    this.dataService.userlogin(this.formlogin.value.username, this.formlogin.value.password).subscribe(data => {
+      const redirect = this.dataService.redirectUrl ? this.dataService.redirectUrl : '/product-management';
+      this.router.navigate([redirect]);
+    }, error => {
+      alert("User name or password is incorrect")
+    });
+  }
+
+  get username() { return this.formlogin.get('username'); }
+  get password() { return this.formlogin.get('password'); }
 
   ngOnInit(): void {
   }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { RouterModule } from '@angular/router';
   declarations: [
     LoginComponent
   ],
-  imports: [
+  imports: [    
+    ReactiveFormsModule,
     CommonModule,
     RouterModule.forChild([
       { path: '', component: LoginComponent },
